cf66266c8e3773f70b06598a6f657e77d4214f45
ellina-tatarintseva <ellina.tatarintseva@gmail.com>
2014-01-23 17:55:46 +0700

    новый интерфейс

---
+++ Association/src/com/example/association/Regulations.java
@@ -0,0 +1,29 @@
+package com.example.association;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+//import android.support.v4.widget.SimpleCursorAdapter.ViewBinder;
+import android.view.Menu;
+//import android.view.View;
+
+public class Regulations extends Activity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	
+		setContentView(R.layout.view_regulations);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+		
+		
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
